3a982783909dfce45ba7d4791cfc4d9346cfba1a
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2021-06-11 23:53:29 -0300

    Commit inicial

--- docs/service-worker.js
+++
@@ -1,1 +0,0 @@
-const cacheName="acw-v1",assets=["./","./index.html","./src.bundle.js","./assets/logo.svg","./favicon.png"];self.addEventListener("install",(e=>{e.waitUntil(caches.open("acw-v1").then((e=>e.addAll(assets))))})),self.addEventListener("fetch",(e=>{e.respondWith(caches.match(e.request,{ignoreSearch:!0}).then((s=>s||fetch(e.request))))}));
